1a307552221d29735d75f27b1d79cda3236333f0
Florian Krämer <florian.kraemer@cakedc.com>
2016-06-02 12:16:03 +0200

    Adding another implementation

--- tests/TestCase/GeoIpTraitTest.php
+++ tests/TestCase/GeoIpTraitTest.php
@@ -15,34 +15,45 @@ class TestGeoIpTrait {
  */
 class GeoIpTraitTest extends TestCase {
 
+    public function testGeoIpLegacy() {
+        $gi = geoip_open(ROOT . DS . 'data' . DS . 'GeoLiteCity.dat', GEOIP_STANDARD);
+        echo geoip_country_code_by_addr($gi, "24.24.24.24") . "\t" .
+             geoip_country_name_by_addr($gi, "24.24.24.24") . "\n";
+
+        $gi = geoip_open(ROOT . DS . 'data' . DS . 'GeoLiteCityv6.dat', GEOIP_STANDARD);
+        echo geoip_country_code_by_addr($gi, "2a00:1a48:7807:103:be76:4eff:fe08:8a20") . "\t" .
+             geoip_country_name_by_addr($gi, "2a00:1a48:7807:103:be76:4eff:fe08:8a20") . "\n";
+    }
+
+    public function testGeoIpV2() {
+        return;
+
+
+        // This creates the Reader object, which should be reused across
+        // lookups.
+        $reader = new Reader(ROOT . DS . 'data' . DS . 'GeoLiteCity.dat');
+
+        // Replace "city" with the appropriate method for your database, e.g.,
+        // "country".
+        $record = $reader->city('128.101.101.101');
+
+        print($record->country->isoCode . "\n"); // 'US'
+        print($record->country->name . "\n"); // 'United States'
+        print($record->country->names['zh-CN'] . "\n"); // '美国'
+
+        print($record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
+        print($record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
+
+        print($record->city->name . "\n"); // 'Minneapolis'
+
+        print($record->postal->code . "\n"); // '55455'
+
+        print($record->location->latitude . "\n"); // 44.9733
+        print($record->location->longitude . "\n"); // -93.2323
+    }
+
     public function testLookup()
     {
-
-
-//    // This creates the Reader object, which should be reused across
-//    // lookups.
-//    $reader = new Reader(ROOT . DS . 'data' . DS . 'GeoLiteCity.dat');
-//
-//    // Replace "city" with the appropriate method for your database, e.g.,
-//    // "country".
-//    $record = $reader->city('128.101.101.101');
-//
-//    print($record->country->isoCode . "\n"); // 'US'
-//    print($record->country->name . "\n"); // 'United States'
-//    print($record->country->names['zh-CN'] . "\n"); // '美国'
-//
-//    print($record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
-//    print($record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
-//
-//    print($record->city->name . "\n"); // 'Minneapolis'
-//
-//    print($record->postal->code . "\n"); // '55455'
-//
-//    print($record->location->latitude . "\n"); // 44.9733
-//    print($record->location->longitude . "\n"); // -93.2323
-//die();
-
-
         $trait = new TestGeoIpTrait();
         $result = $trait->getLocationFromIp('8.8.8.8');
         debug($result);
